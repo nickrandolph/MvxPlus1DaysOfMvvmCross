3039c8317f285f1ac478ab960a5ed86057616610
Nick Randolph <nick@builttoroam.com>
2018-04-16 17:49:55 +1000

    Tidying up Android Forms

--- Mvx-00-FirstDemo/FirstDemo.Forms/Forms.Droid/MainActivity.cs
+++ Mvx-00-FirstDemo/FirstDemo.Forms/Forms.Droid/MainActivity.cs
@@ -1,37 +1,15 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-using MvvmCross.Core;
+using FirstDemo.Core.ViewModels;
 using MvvmCross.Forms.Platforms.Android.Core;
 using MvvmCross.Forms.Platforms.Android.Views;
-using MvvmCross.Platforms.Android.Core;
-using FirstDemo.Core.ViewModels;
 
 namespace FirstDemo.Forms.Droid
 {
     [Activity(Label = "FirstDemo.Forms", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : MvxFormsAppCompatActivity<MainViewModel>
-    {
-        public MainActivity() : base()
-        {
-            this.RegisterSetupType<CustomSetup>();
-        }
-    }
-
-    public class CustomSetup : MvxFormsAndroidSetup<Core.App, FirstDemo.Forms.App>
+    public class MainActivity : MvxFormsAppCompatActivity<MvxFormsAndroidSetup<Core.App, App>, Core.App, App, MainViewModel>
     {
     }
-
-    //public class Monitor : IMvxSetupMonitor
-    //{
-    //    public void InitializationComplete()
-    //    {
-    //    }
-    //}
 }
 
