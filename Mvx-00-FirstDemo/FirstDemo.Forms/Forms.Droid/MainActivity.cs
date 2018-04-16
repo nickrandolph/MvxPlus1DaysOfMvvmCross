53df66d324fff0ab9567aa298cac7c5998ddf212
Nick Randolph <nick@builttoroam.com>
2018-04-17 08:10:15 +1000

    Adding Droid XForms splash screen

--- Mvx-00-FirstDemo/FirstDemo.Forms/Forms.Droid/MainActivity.cs
+++ Mvx-00-FirstDemo/FirstDemo.Forms/Forms.Droid/MainActivity.cs
@@ -2,14 +2,16 @@
 using Android.App;
 using Android.Content.PM;
 using FirstDemo.Core.ViewModels;
-using MvvmCross.Forms.Platforms.Android.Core;
 using MvvmCross.Forms.Platforms.Android.Views;
 
 namespace FirstDemo.Forms.Droid
 {
-    [Activity(Label = "FirstDemo.Forms", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : MvxFormsAppCompatActivity<MvxFormsAndroidSetup<Core.App, App>, Core.App, App, MainViewModel>
+    [Activity(Label = "FirstDemo.Forms", Icon = "@mipmap/icon", Theme = "@style/MainTheme",
+        // MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : MvxFormsAppCompatActivity<MainViewModel>
     {
     }
+
 }
 
---
+++ Mvx-00-FirstDemo/FirstDemo.Forms/Forms.Droid/SplashScreen.cs
@@ -0,0 +1,26 @@
+﻿
+using Android.App;
+using Android.OS;
+using MvvmCross.Forms.Platforms.Android.Core;
+using MvvmCross.Forms.Platforms.Android.Views;
+
+namespace FirstDemo.Forms.Droid
+{
+    [Activity(Label = "FirstDemo.Forms.Splash", Theme = "@style/MainTheme", 
+        MainLauncher = true, NoHistory = true)]
+    public class SplashScreen : MvxFormsSplashScreenAppCompatActivity<MvxFormsAndroidSetup<Core.App, App>, Core.App, App>
+    {
+        public SplashScreen()
+             : base(Resource.Layout.SplashScreen)
+        {
+        }
+
+        protected override void RunAppStart(Bundle bundle)
+        {
+            StartActivity(typeof(MainActivity));
+            base.RunAppStart(bundle);
+        }
+    }
+
+}
+
